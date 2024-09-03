dbb86863c51945a94997869ec59665a67093b1e8
sonack12 <wogh8615@gmail.com>
2024-09-03 13:47:16 +0900

    Auto commit for file: ./web_workspace/0903/ex/ex04.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0903/ex/ex04.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+
+import { ref } from 'vue';
+
+const num = ref();
+const ans = Math.floor(Math.random() * 100) + 1
+console.log(ans);
+</script>
+
+<template>
+    <h1>1 to 100</h1>
+    <input type="number" v-model="num">
+    <div v-if="num === undefined">1 부터 100 사이 숫자를 입력하세요</div>
+    <div v-else-if="num > 100 || num < 1">1 부터 100 까지의 수가 아닙니다</div>
+    <div v-else-if="num === ans">정답</div>
+    <div v-else-if="num <= ans">UP</div>
+    <div v-else-if="num >= ans">DOWN</div>
+    
+</template>
+
+<style scoped>
+
+</style>s
