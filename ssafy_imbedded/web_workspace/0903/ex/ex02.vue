66011cfc00ec577ecbd2bc7ba846307bf8ff428f
sonack12 <wogh8615@gmail.com>
2024-09-03 11:40:17 +0900

    Auto commit for file: ./web_workspace/0903/ex/ex02.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0903/ex/ex02.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import { ref } from 'vue';
+const lottoNums = ref([ 0, 0, 0, 0, 0, 0 ]);
+function createNum() {
+    for (let i = 0; i < 6; ++i) {
+        let num = Math.floor(Math.random() * 45) + 1;
+        if (lottoNums.value.includes(num)) {
+            while(lottoNums.value.includes(num)) {
+                num = Math.floor(Math.random() * 45) + 1;
+            }               
+        }
+        lottoNums.value[i] = num; 
+    }
+    lottoNums.value.sort((a, b) => a - b);
+}
+
+</script>
+
+<template>
+    <h1>로또 번호 생성기</h1>
+    <button @click="createNum">번호 생성</button>
+    <div>{{ lottoNums }}</div>
+</template>
+
+<style scoped>
+
+</style>
