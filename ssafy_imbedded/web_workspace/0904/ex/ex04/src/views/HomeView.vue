59e6936e1357c801b3b607aac4ba16d13d3ce919
sonack12 <wogh8615@gmail.com>
2024-09-04 13:28:53 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex04/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex04/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { usePersonStore } from '@/stores/person';
+import Homechild from '@/components/Homechild.vue';
+
+const personStore = usePersonStore();
+</script>
+
+<template>
+    <h1>게시판</h1>
+    <p>이름: {{ personStore.name }}</p>
+    <p>나이: {{ personStore.age }}</p>
+    <button @click="personStore.decreaseAge">나이를 줄이자</button>
+    <Homechild></Homechild>
+</template>
+
+<style scoped>
+</style>
