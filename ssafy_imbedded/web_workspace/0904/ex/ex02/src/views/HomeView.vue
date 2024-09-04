2313f716251cb64245e35ec8671c4e2cf0e0ee1a
sonack12 <wogh8615@gmail.com>
2024-09-04 11:23:53 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex02/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex02/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import HomeHeader from "@/components/HomeHeader.vue";
+import HomeMain from "@/components/HomeMain.vue";
+import HomeFooter from "@/components/HomeFooter.vue";
+
+</script>
+
+<template>
+    <h1>Home</h1>
+
+    <HomeHeader></HomeHeader>
+    <HomeMain></HomeMain>
+    <HomeFooter></HomeFooter>
+</template>
+
+<style scoped>
+
+</style>
