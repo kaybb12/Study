2c9a223e9c9522125022bbc3b1d7ec3b4d2dff21
sonack12 <wogh8615@gmail.com>
2024-09-04 13:34:31 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex05/src/components/FriedChicken.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex05/src/components/FriedChicken.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import ChickenChild from './ChickenChild.vue';
+
+const props = defineProps({
+  title: String,
+  salt: Number,
+});
+
+const emit = defineEmits(['update-salt']);
+
+const updateSalt = (newSalt) => {
+  emit('update-salt', newSalt);
+};
+</script>
+
+<template>
+    <div>
+      <h2>FriedChicken</h2>
+      <p>Title: {{ title }}</p>
+      <p>Salt: {{ salt }}</p>
+      <ChickenChild :title="title" :salt="salt" @update-salt="updateSalt" />
+    </div>
+</template>
+  
+  
