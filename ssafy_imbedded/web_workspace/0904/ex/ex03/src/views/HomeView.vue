98caf0b1875250f8b81491349e0db1530381e1b0
sonack12 <wogh8615@gmail.com>
2024-09-04 13:08:58 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex03/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex03/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import { ref } from 'vue';
+import Detail from '@/components/Detail.vue';
+
+const posts = ref([
+    { id: 1, title: '즐거운 싸피' },
+    { id: 2, title: 'vue 할만함?' },
+    { id: 3, title: '임베 화이팅' },
+]);
+</script>
+
+<template>
+    <h1>게시판</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <router-link :to="{ name: 'Detail', params: { id: post.id }}">{{ post.title }}</router-link>
+      </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
