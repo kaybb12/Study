4d6e29055b870d6e48774f36787f25115db7c8ae
sonack12 <wogh8615@gmail.com>
2024-09-04 11:28:13 +0900

    Auto commit for file: ./web_workspace/0904/class/vue-project/src/components/HomeChild.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/class/vue-project/src/components/HomeChild.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+import GrandChild from './GrandChild.vue';
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
+console.log(counter.count);
+counter.increment();
+counter.increment();
+
+const props = defineProps({
+    nameProp: String,
+    ageProp: Number,
+})
+
+const emit = defineEmits(["emitTest"]);
+
+function changeName() {
+    emit("emitTest", "자잔");
+}
+
+</script>
+
+<template>
+    <h1>HomeChild</h1>
+    <div>count : {{ counter.count }}</div>
+    <div>{{ nameProp }}</div>
+    <div>{{ ageProp }}</div>
+    <button @click="changeName(a)">부모에게 요청하기</button>
+    <GrandChild :nameProp="nameProp" :ageProp="ageProp"></GrandChild>
+</template>
+
+<style scoped>
+
+</style>
