00c265b7f10d19f4e908421c1b4515f477197b05
sonack12 <wogh8615@gmail.com>
2024-09-04 13:36:15 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex05/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex05/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import FriedChicken from '@/components/FriedChicken.vue';
+
+const title = ref("치킨은 맛있다");
+const salt = ref(30);
+
+const updateSalt = (newSalt) => {
+  salt.value = newSalt;
+};
+</script>
+
+<template>
+    <div>
+      <h1>HomeView</h1>
+      <FriedChicken :title="title" :salt="salt" @update-salt="updateSalt" />
+    </div>
+  </template>
+
+  
