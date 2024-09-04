bf17a7be54c5c9d6356d0df6c97aa5a989926b17
sonack12 <wogh8615@gmail.com>
2024-09-04 11:21:31 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex01/src/App.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex01/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink :to="{name : 'home' }">Home</RouterLink>
+        <span> | </span>
+        <RouterLink :to="{name : 'about' }">About</RouterLink>
+
+      </nav>
+  </header>
+  <RouterView />
+</template>
