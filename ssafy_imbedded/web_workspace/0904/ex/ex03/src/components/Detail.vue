ee800ae6296d6c4625167e54a3f0a749835af589
sonack12 <wogh8615@gmail.com>
2024-09-04 13:19:24 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex03/src/components/Detail.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex03/src/components/Detail.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+function goBack() {
+    router.push({name : "home"});
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ $route.params.id }}번 글 상세페이지</h1>
+        <button @click="goBack">뒤로가기</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
