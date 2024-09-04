a7358c8367c038a6bf4a721c1f4a2f9dbc2d3bf7
sonack12 <wogh8615@gmail.com>
2024-09-04 11:27:43 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex06/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex06/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import HomeChild from '@/components/HomeChild.vue';
+import { ref } from 'vue';
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
+console.log(counter.count);
+counter.increment();
+counter.increment();
+
+
+const name = ref("david");
+const age = ref(10);
+
+function changeName(n){
+    console.log(n);
+    name.value = n;
+    console.log("comein")
+}
+
+</script>
+
+<template>
+    <h1>Home</h1>
+    <div>count : {{ counter.count }}</div>
+    <div>{{ name }}</div>
+    <div>{{ age }}</div>
+    
+    <HomeChild
+    @emitTest="changeName"
+    :nameProp="name" 
+    :ageProp="age"
+    ></HomeChild>
+
+</template>
+
+<style scoped>
+
+</style>
