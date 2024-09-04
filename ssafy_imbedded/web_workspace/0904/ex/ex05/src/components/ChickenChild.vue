ccf6da79e0083a44f8abfe0a8d26b7717752a8df
sonack12 <wogh8615@gmail.com>
2024-09-04 13:35:01 +0900

    Auto commit for file: ./web_workspace/0904/ex/ex05/src/components/ChickenChild.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0904/ex/ex05/src/components/ChickenChild.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  title: String,
+  salt: Number,
+});
+
+const emit = defineEmits(['update-salt']);
+
+const changeSalt = () => {
+  emit('update-salt', 20);
+};
+</script>
+
+<template>
+    <div>
+      <h3>ChickenChild</h3>
+      <p>Title: {{ title }}</p>
+      <p>Salt: {{ salt }}</p>
+      <button @click="changeSalt">소금을 20으로 변경</button>
+    </div>
+</template>
+  
