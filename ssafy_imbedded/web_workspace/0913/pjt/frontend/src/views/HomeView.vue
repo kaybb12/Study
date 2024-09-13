ecad9c6702d27800fffd89d71dcbef12f5d89334
sonack12 <wogh8615@gmail.com>
2024-09-13 14:27:00 +0900

    Auto commit for file: ./web_workspace/0913/pjt/frontend/src/views/HomeView.vue with last modified date

---
+++ ssafy_imbedded/web_workspace/0913/pjt/frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import LineChart from "@/components/LineChart.vue";
+import UserForm from "@/components/UserForm.vue";
+import { useChartStore } from "@/stores/chart.js";
+
+const chartStore = useChartStore();
+
+console.log("makeChart 함수 실행");
+chartStore.makeChart();
+</script>
+
+<template>
+ <LineChart />
+ <UserForm />
+</template>
