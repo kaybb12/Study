725c61cf6c52bebf4cc540caa1f8c6801b5039ae
sonack12 <wogh8615@gmail.com>
2024-09-27 11:36:06 +0900

    Auto commit for file: ./embedec_workspace/0927/ex02/1.c with last modified date

---
+++ ssafy_imbedded/embedec_workspace/0927/ex02/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main() {
+
+	int a = 10;
+	char b = 'A';
+
+	void* pa = &a;
+	void* pb = &b;
+
+	printf("%d\n", *(int*)pa);
+	printf("%c\n", *(char*)pb);
+
+	return 0;
+}
