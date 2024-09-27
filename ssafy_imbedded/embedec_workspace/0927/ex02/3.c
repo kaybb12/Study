fe89a66b615f987972ce882726208d7baa7f7955
sonack12 <wogh8615@gmail.com>
2024-09-27 11:35:22 +0900

    Auto commit for file: ./embedec_workspace/0927/ex02/3.c with last modified date

---
+++ ssafy_imbedded/embedec_workspace/0927/ex02/3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+struct Node {
+    int y;
+    int x;
+};
+
+void abc(void* v){
+
+    struct Node val = *(struct Node*)v;
+
+    printf("%d %d\n", val.y, val.x);
+}
+
+int main(){
+
+    struct Node bbq = { .y = 10, .x = 20 };
+    abc(&bbq);
+    return 0;
+}
