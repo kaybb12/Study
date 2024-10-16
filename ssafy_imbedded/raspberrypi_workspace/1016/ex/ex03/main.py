e1d2e7a29bad430fbaaa971d8f4915c467745cad
sonack12 <wogh8615@gmail.com>
2024-10-16 11:18:58 +0900

    Auto commit for file: ./raspberrypi_workspace/1016/ex/ex03/main.py with last modified date

---
+++ ssafy_imbedded/raspberrypi_workspace/1016/ex/ex03/main.py
@@ -0,0 +1,17 @@
+# This Python file uses the following encoding: utf-8
+# import sys
+# from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    arr1 = [[i + j * 5 + 1 for i in range(5)] for j in range(5)]
+    print("1")
+    print(arr1)
+
+    arr2 = [[ 25 - (i + j * 5) for i in range(5)] for j in range(5)]
+    print("2")
+    print(arr2)
+
+    while(True):
+        pass;
+
+
