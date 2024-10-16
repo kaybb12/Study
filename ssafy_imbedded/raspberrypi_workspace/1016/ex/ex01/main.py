2de26e952ad6388bda8beea0b7b50a0ca035cfcd
sonack12 <wogh8615@gmail.com>
2024-10-16 10:47:00 +0900

    Auto commit for file: ./raspberrypi_workspace/1016/ex/ex01/main.py with last modified date

---
+++ ssafy_imbedded/raspberrypi_workspace/1016/ex/ex01/main.py
@@ -0,0 +1,14 @@
+# This Python file uses the following encoding: utf-8
+# import sys
+# from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    a = input()
+    print("1. ", a)
+    print("2. ", (int)(a) + 10)
+    a,b = input().split()
+    print("3. ", a,b)
+    print("4.")
+    for _ in range(10):
+        print(7)
+
