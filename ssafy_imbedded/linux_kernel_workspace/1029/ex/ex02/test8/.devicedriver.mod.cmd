571feef115244828800399966941574e90410155
sonack12 <wogh8615@gmail.com>
2024-10-29 10:34:57 +0900

    Auto commit for file: ./linux_kernel_workspace/1029/ex/ex02/test8/.devicedriver.mod.cmd with last modified date

---
+++ ssafy_imbedded/linux_kernel_workspace/1029/ex/ex02/test8/.devicedriver.mod.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/pi/workspace/1029/class/test8/devicedriver.mod := printf '%s\n'   devicedriver.o | awk '!x[$$0]++ { print("/home/pi/workspace/1029/class/test8/"$$0) }' > /home/pi/workspace/1029/class/test8/devicedriver.mod
